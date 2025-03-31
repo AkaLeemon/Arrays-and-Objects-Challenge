5597ba8e3c2e77d56916b3e214bdff66e2c39b78
Te Piha <54650178+Te-Piha@users.noreply.github.com>
2025-03-31 22:53:33 +1300

    update challenges

--- arrays.js
+++ arrays.js
@@ -1,40 +1,66 @@
 
 // 1️⃣ Create an array called numbers that contains the numbers 1 to 5
-const numbers = [1, 2, 3, 4, 5];
+
 
 // 2️⃣ Create a function called doubleArray that doubles each number in an array
-function doubleArray(arr) {
-    return arr.map(num => num * 2);
-}
+
 
 // 3️⃣ Create a function called sumArray that returns the sum of all numbers in an array
-function sumArray(arr) {
-    return arr.reduce((sum, num) => sum + num, 0);
-}
+
 
 // 4️⃣ Create a function called findMax that returns the largest number in an array
-function findMax(arr) {
-    return Math.max(...arr);
-}
+
 
 // 5️⃣ Create a function called removeEvens that removes even numbers from an array
-function removeEvens(arr) {
-    return arr.filter(num => num % 2 !== 0);
-}
+
 
 // 6️⃣ Create a function called reverseArray that reverses the order of elements in an array
-function reverseArray(arr) {
-    return arr.reverse();
-}
+
 
 // 7️⃣ (Hard) Create a function called flattenArray that flattens a nested array
-function flattenArray(arr) {
-    return arr.flat(Infinity);
-}
 
 // 8️⃣ (Hard) Create a function called uniqueElements that returns only unique values from an array
-function uniqueElements(arr) {
-    return [...new Set(arr)];
-}
+
 
 module.exports = { numbers, doubleArray, sumArray, findMax, removeEvens, reverseArray, flattenArray, uniqueElements };
+
+
+// hints below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// still below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2 .map
+// 3 .reducer
+// 4 .Math.max
+// 5 .filter
+// 6 ....surely you know this
+// 7 .flat
+// 8 ...new Set
--- objects.js
+++ objects.js
@@ -1,41 +1,49 @@
-// 1️⃣ Create an object called person with properties name and age
-const person = { name: "John", age: 30 };
+const { constants } = require("buffer");
 
-// 2️⃣ Create a function called getProperty that returns the value of a property in an object
-function getProperty(obj, prop) {
-    return obj[prop];
-}
+// 1️⃣ Create an object called person with properties name and age
+
+// 2️⃣ Create a function called getProperty that returns the value of a property(specify a propery) in an object
 
 // 3️⃣ Create a function called addProperty that adds a new property to an object
-function addProperty(obj, key, value) {
-    obj[key] = value;
-    return obj;
-}
+
 
 // 4️⃣ Create a function called removeProperty that removes a property from an object
-function removeProperty(obj, key) {
-    delete obj[key];
-    return obj;
-}
+
 
 // 5️⃣ Create a function called objectKeys that returns an array of object keys
-function objectKeys(obj) {
-    return Object.keys(obj);
-}
 
 // 6️⃣ Create a function called objectValues that returns an array of object values
-function objectValues(obj) {
-    return Object.values(obj);
-}
+
 
 // 7️⃣ (Hard) Create a function called mergeObjects that merges two objects into one
-function mergeObjects(obj1, obj2) {
-    return { ...obj1, ...obj2 };
-}
 
 // 8️⃣ (Hard) Create a function called deepClone that creates a deep copy of an object
-function deepClone(obj) {
-    return JSON.parse(JSON.stringify(obj));
-}
+
 
 module.exports = { person, getProperty, addProperty, removeProperty, objectKeys, objectValues, mergeObjects, deepClone };
+
+
+
+// hints below
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 5 .keys
+//6 .values
+// 7 "the spread opeartor" + the objects ...obj1, ...obj2
+// 8 JSON.parse + JSON.stringify
